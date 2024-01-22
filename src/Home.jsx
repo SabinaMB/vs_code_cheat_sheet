e6d4fa769409aeebb34b44d9aa7ce5ada14f3e74
SabinaMB <sabina.marian.blaariu@gmail.com>
2024-01-22 13:40:21 +0100

    footer

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,18 +5,33 @@ import "./Home.css";
 function Home() {
   return (
     <div className="home-container">
-      <h3 className="title">
-        Visual Studio Code keyboard shortcut reference sheet for your platform
+
+      <header>
+      <h1 className="main-title">VS Code Cheat Sheet</h1>
+        <p className="sub-title">
+          Your guide to Visual Studio Code keyboard shortcuts
+        </p>
+      </header>
+
+      <div className="content">
+      
+      <h3 className="section-title">
+      Choose your platform to view shortcuts:
       </h3>
+
       <div className="homepage-options">
         <Link to="/windows" className="shortcut-button">
-          Windows ShortCut
+          Windows shortcut
         </Link>
         <Link to="/mac" className="shortcut-button">
-          Mac ShortCut
+          Mac shortcut
         </Link>
+        </div>
+        
       </div>
+
     </div>
+
   );
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import "./Footer.css"
+
 
 const Footer = () => {
 return (
     <footer>
         <div className="footer-container">
-            <p>&copy; {new Date().getFullYear()}ShortCut App</p>
+         
+          
+          <span className="created-by">
+             <p>&copy; {new Date().getFullYear()} Created by: Anna, Antonio, Naeem, Sabina</p>
+             </span>
 
         </div>
         </footer>
