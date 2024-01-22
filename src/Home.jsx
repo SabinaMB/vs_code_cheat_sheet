2f2a36c727f8cb9382103d86b6f135997f4eaf21
SabinaMB <sabina.marian.blaariu@gmail.com>
2024-01-22 13:49:39 +0100

    added vscode icon

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
+import vscodeIcon from "./assets/vscode.png";
 
 function Home() {
   return (
     <div className="home-container">
 
       <header>
+      <img src={vscodeIcon} alt="VS Code Icon" className="vscode-icon" />
       <h1 className="main-title">VS Code Cheat Sheet</h1>
         <p className="sub-title">
           Your guide to Visual Studio Code keyboard shortcuts
